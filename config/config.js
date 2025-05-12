4fed34602e6e8a276e3c179314201bcaa60e56b8
Diego Sanchez Tafur <diegosancheztafur3@gmail.com>
2025-05-12 02:03:46 -0500

    segundoCommit

--- config/config.js
+++ config/config.js
@@ -14,5 +14,16 @@ module.exports = {
         rejectUnauthorized: false, // Deshabilitar validación de certificado
       }
     }
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',  // Usar la variable de entorno para la URL completa de la base de datos
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,  // Habilitar SSL
+        rejectUnauthorized: false, // Deshabilitar validación de certificado
+      }
+    }
   }
 };
+  
